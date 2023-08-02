66a89239f1f44adcfc852da530a8c1d42d5e35db
Henning Leutz <leutz@pcsg.de>
2023-08-02 14:40:39 +0200

    style: psr12 code style

--- src/QUI/ERP/Tags/FieldFrontendView.php
+++ src/QUI/ERP/Tags/FieldFrontendView.php
@@ -8,6 +8,9 @@ namespace QUI\ERP\Tags;
 
 use QUI;
 
+use function htmlspecialchars;
+use function is_array;
+
 /**
  * Class FieldFrontendView
  *
@@ -29,12 +32,12 @@ class FieldFrontendView extends QUI\ERP\Products\Field\View
         $tagHtml = '';
 
         $value = $this->getValue();
-        $title = \htmlspecialchars($this->getTitle());
-        $lang  = QUI::getLocale()->getCurrent();
+        $title = htmlspecialchars($this->getTitle());
+        $lang = QUI::getLocale()->getCurrent();
 
         $TagManager = new \QUI\Tags\Manager(QUI::getRewrite()->getProject());
 
-        if (\is_array($value) && isset($value[$lang])) {
+        if (is_array($value) && isset($value[$lang])) {
             foreach ($value[$lang] as $tag) {
                 try {
                     $tagData = $TagManager->get($tag['tag']);
@@ -42,15 +45,15 @@ class FieldFrontendView extends QUI\ERP\Products\Field\View
                     continue;
                 }
 
-                $tagHtml .= '<div class="qui-tags-tag">'.$tagData['title'].'</div>';
+                $tagHtml .= '<div class="qui-tags-tag">' . $tagData['title'] . '</div>';
             }
         }
 
         return '<div class="quiqqer-product-field" 
             data-qui="package/quiqqer/productstags/bin/controls/frontend/FieldFrontendView"
         >
-            <div class="quiqqer-product-field-title">'.$title.'</div>  
-            <div class="quiqqer-product-field-value">'.$tagHtml.'</div>
+            <div class="quiqqer-product-field-title">' . $title . '</div>  
+            <div class="quiqqer-product-field-value">' . $tagHtml . '</div>
         </div>';
     }
 }
